55e946a58619c927ee83bfb04562951e5bb1f60d
Ryan Combest <crcombest@gmail.com>
2022-11-09 11:04:04 -0600

    changes about and home jsx rendered text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,9 @@
 function About() {
-  return (  
-    <h1>About</h1>
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>My name is Ryan Combest, I'm a fullstack software engineer with a passion for the backend and who seeks to bring creative and technical finesse with every post request.</p>
+    </div>  
   )
 }
 
