5298dc52f5d197bb88186888f53e33b45c1b2b60
Ryan Combest <crcombest@gmail.com>
2022-11-11 17:08:48 -0600

    adds text content and links to li, gh, and email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,9 @@
 function Contact() {
-  return (  
-    <h1>Contact</h1>
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Feel free to reach out to me via <a href="https://www.linkedin.com/in/ryan-combest/">LinkedIn</a>, <a href="https://github.com/Rcombest">GitHub</a>, or <a href="mailto: crcombest@gmail.com">email</a>!</p>
+    </div>
   )
 }
 
