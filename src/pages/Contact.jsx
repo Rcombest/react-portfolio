93756327eb8bf836c72febfb112d64df8f02c1d6
Ryan Combest <crcombest@gmail.com>
2022-11-20 20:28:12 -0600

    makes p tag on contact page into an h2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,13 @@ function Contact() {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Feel free to reach out to me via <a href="https://www.linkedin.com/in/ryan-combest/">LinkedIn</a>, <a href="https://github.com/Rcombest">GitHub</a>, or <a href="mailto: crcombest@gmail.com">email</a>!</p>
+      <h2>Feel free to reach out to me via 
+        <a href="https://www.linkedin.com/in/ryan-combest/">LinkedIn</a>, 
+        <a href="https://github.com/Rcombest">GitHub</a>, 
+        or 
+        <a href="mailto: crcombest@gmail.com">email</a>
+        !
+      </h2>
     </div>
   )
 }
