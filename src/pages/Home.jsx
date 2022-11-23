18dbc4f988cd7c0ce17a4f597cbcf316fac0b61b
Ryan Combest <crcombest@gmail.com>
2022-11-22 22:00:46 -0600

    adds pic to home page and styles it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import styles from "./Home.module.css"
+
 function Home() {
   return ( 
-    <div id="home-about">
+    <div className={styles.homeAbout}>
       <h1> Ryan Combest </h1>
+      <img src="./HomePic.jpeg" alt="Ryan Combest" className={styles.homeImg} />
       <h2> Software Engineer </h2>
       <p>Music lover, coffee enjoyer, philosophy nerd.</p>
     </div>
